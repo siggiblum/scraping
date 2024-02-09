ac43178590e3366b630df964eac205a1dc9a6fa7
Siggi Blum <blumensb@dukes.jmu.edu>
2024-02-09 09:09:24 -0500

    on ready

--- on.py
+++ on.py
@@ -4,41 +4,67 @@ import requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-vorur = {}
+vorur = []
 urls = ["https://www.on.is/verdskra/"]
-for i in range(2, 50):
-    urls.append(f"{urls[0]}?page={i}")
+
 
 for url in urls:
     try:
         req = requests.get(url, headers=headers)
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-
         data = soup.find_all("div", class_="col-lg-8 offset-lg-2")
         for row in data:
-            tr = row.find("tr")
-            for tri in tr:
-                name, price = None, None
-                # Find all <td> tags within the table row
-                tds = tr.find("td")
-                print(tds.text.strip())
-                # print(tds)
-                # for td in tds:
-                #     # Check if the <td> has a 'style' attribute
-                #     if 'style' in td.attrs:
-                #         # If it has a style, treat it as price
-                #         price = td.text.strip()
-                #     else:
-                #         # If no style, treat it as name
-                #         name = td.text.strip()
-
-                # # If both name and price are found, add them to the dictionary
-                
-                # if name and price:
-                #     vorur[name] = price
-                    # print(name, price)
+            more = soup.find_all("tr")
+            for da in more:
+                allt = da.find_all("td")
+                temp = {}
+                if not allt[0].find("strong"):
+                    if(len(allt) == 4):
+                        nafn = allt[0].text.strip()
+                        verd1 = allt[3].text.strip()
+                        temp["Nafn"] = nafn
+                        temp["Price"] = verd1
+                    if(len(allt) == 3):
+                        nafn = allt[0].text.strip()
+                        verd1 = allt[1].text.strip()
+                        verd2 = allt[2].text.strip()
+                        temp["Nafn"] = nafn
+                        temp["Price"] = verd1
+                        temp["Price2"] = verd2
+                    if(len(allt) == 2):
+                        nafn = allt[0].text.strip()
+                        verd1 = allt[1].text.strip()
+                        temp["Nafn"] = nafn
+                        temp["Price"] = verd1
+                vorur.append(temp)
     except Exception as e:
         print(f"Error processing {url}: {e}")
 
-driver.quit()
+vorur1 = []
+count = 0
+for i in vorur:
+    if(len(i)!=0):
+        if("Almenn rafmag" in i["Nafn"]):
+            count += 1
+        if(count != 2):
+            vorur1.append(i)
+        else:
+            break
+
+for i in vorur1:
+    if(len(i) == 2):
+        spl = i["Price"].split(" ")
+        price = spl[0].replace(",", '.')
+        i["Price"] = price
+    if(len(i) == 3):
+        spl = i["Price"].split(" ")
+        price = spl[0].replace(",", '.')
+        i["Price"] = price
+
+        spl = i["Price2"].split(" ")
+        price = spl[0].replace(",", '.')
+        i["Price2"] = price
+
+for i in vorur1:
+    print(i)
